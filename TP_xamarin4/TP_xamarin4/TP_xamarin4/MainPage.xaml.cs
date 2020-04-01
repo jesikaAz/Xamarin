5800c81b994c899286c6db5f94e409a8bcc0281d
jesikaAz <j.anschutz@laposte.net>
2020-04-01 13:51:16 +0200

    init
    add partie3 on partie4

---
+++ TP_xamarin4/TP_xamarin4/TP_xamarin4/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace TP_xamarin4
+{
+
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Connection_Clicked(object sender, EventArgs e)
+        {
+            String identifiant = this.identifiant.Text;
+            String password = this.password.Text;
+            Boolean isRemember = this.remember.IsToggled;
+
+            this.errorIdentifiant.IsVisible = false;
+            this.errorPassword.IsVisible = false;
+
+            bool errorExist = false;
+            if (String.IsNullOrEmpty(identifiant) || identifiant.Length < 3)
+            {
+                errorExist = true;
+                this.errorIdentifiant.IsVisible = true;
+            }
+
+            if (String.IsNullOrEmpty(password) || password.Length < 6)
+            {
+                errorExist = true;
+                this.errorPassword.IsVisible = true;
+            }
+
+            if (errorExist) { return; }
+            this.tweets.IsVisible = true;
+            this.forms.IsVisible = false;
+            return;
+        }
+    }
+}
