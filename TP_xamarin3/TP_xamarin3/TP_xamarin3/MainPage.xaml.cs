38b445af0ce79ab9e358bf2dcd7ac8e50eba6449
jesikaAz <j.anschutz@laposte.net>
2020-04-01 12:06:18 +0200

    partie3

--- TP_xamarin3/TP_xamarin3/TP_xamarin3/MainPage.xaml.cs
+++ TP_xamarin3/TP_xamarin3/TP_xamarin3/MainPage.xaml.cs
@@ -1,21 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace TP_xamarin3
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
+    
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Connection_Clicked(object sender, EventArgs e) 
+        { 
+            //Console.WriteLine("Connection is clicked");
+            String identifiant = this.identifiant.Text; 
+            String password = this.password.Text; 
+            Boolean isRemember = this.remember.IsToggled;
+
+            this.errorIdentifiant.IsVisible = false;
+            this.errorPassword.IsVisible = false;
+
+            bool errorExist = false;
+            if (String.IsNullOrEmpty(identifiant) || identifiant.Length < 3)
+            {
+                errorExist = true;
+                this.errorIdentifiant.IsVisible = true;
+            }
+
+            if (String.IsNullOrEmpty(password) || password.Length < 6)
+            {
+                errorExist = true;
+                this.errorPassword.IsVisible = true;
+            }
+
+            if (errorExist) { return; }
+            this.tweets.IsVisible = true;
+            this.forms.IsVisible = false;
+            return;
         }
     }
 }
