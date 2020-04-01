819eadd3d5699f09a9976c6480b9b4965b3eb8fa
jesikaAz <j.anschutz@laposte.net>
2020-04-01 10:41:39 +0200

    update
    add partie2 on partie3

---
+++ TP_xamarin3/TP_xamarin3/TP_xamarin3/Utils/ImageFromResources.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TP_xamarin3.Utils
+{
+    [ContentProperty(nameof(Source))]
+    public class ImageFromResources : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            // Do your translation lookup here, using whatever method you require
+            var imageSource = ImageSource.FromResource(Source, typeof(ImageFromResources).GetTypeInfo().Assembly);
+
+            return imageSource;
+        }
+    }
+}
