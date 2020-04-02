37859e7d9af468e67f3e18a9d88241c6316449f6
jesikaAz <j.anschutz@laposte.net>
2020-04-02 16:12:47 +0200

    update
    create Tweet/FormPage on viewsFolder + update NavigationPage and delete MainPage + update services

--- TP_xamarin4/TP_xamarin4/TP_xamarin4/Views/Header.xaml.cs
+++ TP_xamarin4/TP_xamarin4/TP_xamarin4/Views/Header.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace TP_xamarin4
+namespace TP_xamarin4.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Header : ContentView
